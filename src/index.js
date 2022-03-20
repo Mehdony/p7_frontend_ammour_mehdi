c6ff875e51aebc80b54f5a5ecd7f61fe94839e76
Mehdony <86969900+Mehdony@users.noreply.github.com>
2022-03-20 16:32:53 +0100

    Init routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
-import * as serviceWorker from "./serviceWorker"
+
 
 ReactDOM.render(
   <BrowserRouter>
@@ -11,7 +11,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorker.unregister()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

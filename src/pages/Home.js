3e8c63c3cde11864d0bd2a61a08c297d9bc544f7
Mehdony <86969900+Mehdony@users.noreply.github.com>
2022-04-02 17:03:21 +0200

    init navbar ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,29 +3,38 @@ import "../cssComponents/Home.css";
 import CreatePost from "../components/CreatePost/CreatePost";
 import DisplayPost from "../components/DisplayPost/DisplayPost";
 import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function Home() {
 
-  const [posts, setPosts] = useState([]);
-  const username = localStorage.getItem("username");
-  const token = localStorage.getItem("token");
+  const [posts, setPosts] = useState([])
+  const username = localStorage.getItem("username")
+  const token = localStorage.getItem("token")
 
   const navigate = useNavigate();
 
   const handleLogout = (e) => {
-    e.preventDefault();
-    localStorage.removeItem("token");
-    localStorage.removeItem("username");
-    localStorage.removeItem("userId");
-    navigate("/login");
-  };
+    e.preventDefault()
+    localStorage.removeItem("token")
+    localStorage.removeItem("username")
+    localStorage.removeItem("userId")
+    navigate("/login")
+  }
 
   return (
-    <>
+
+      token ? 
+
+    (
+      <>
+      <div className="navBar">
       <button className="logout-button" onClick={handleLogout}>
         logout
       </button>
-
+      <button className="profil-button" onClick={handleLogout}>
+        profil
+      </button>
+      </div>
       <CreatePost setPosts={setPosts} posts={posts} />
       <DisplayPost
         username={username}
@@ -33,7 +42,9 @@ function Home() {
         posts={posts}
         token={token}
       />
-    </>
+    </> ) : (
+            <Navigate to="/login" replace={true} />
+        )
   );
 }
 
